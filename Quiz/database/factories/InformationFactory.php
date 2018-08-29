9cecd7a80166af95d2919b3b5b7321efde547f8f
Kiran <kiran@Kirans-MacBook-Pro.local>
2018-08-29 21:18:46 +0545

    created models and everything

---
+++ Quiz/database/factories/InformationFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Model\Information::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- Quiz/routes/api.php
+++ Quiz/routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/informations','InformationController');
+Route:group(['prefix'=>'informations'],function(){
+    Route::apiResource('/{information}/questions','QuestionController');
+});
